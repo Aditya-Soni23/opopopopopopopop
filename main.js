693a8ec167a639e58ed8d7739b3645ef3f5c1eeb
Aditya-Soni23 <77341618+Aditya-Soni23@users.noreply.github.com>
2021-07-16 16:18:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function one(){
+    window.alert("THE CAR OWNER")
+}
+
+function two(){
+    window.alert("THE KING")
+}
+
+function three(){
+    window.alert("MOHITH SAI'S STYLE")
+}
+
+function four(){
+    window.alert("MOHITH SAI'S NICK NAME (BUNNY)")
+}
+
+function five(){
+    window.alert("THE GANGSTER")
+}
+
+function six(){
+    window.alert("SRI KRISHNA")
+}
+
+function seven(){
+    window.alert("HALLOWEEN")
+}
